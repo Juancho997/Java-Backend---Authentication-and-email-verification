05f6e04c75850b0abd372119ff20f1c8aba8fa19
Juan Bonino <tomasboni9@gmail.com>
2022-10-17 12:22:18 -0300

    feature: user registration and insertion in db

--- src/main/java/com/boninojuan/demo/registration/RegistrationService.java
+++ src/main/java/com/boninojuan/demo/registration/RegistrationService.java
@@ -1,10 +1,32 @@
 package com.boninojuan.demo.registration;
 
+import com.boninojuan.demo.appuser.AppUser;
+import com.boninojuan.demo.appuser.AppUserRole;
+import com.boninojuan.demo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if(!isValidEmail) {
+            throw new IllegalStateException("Invalid email");
+        }
+
+        return appUserService.singUp(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
