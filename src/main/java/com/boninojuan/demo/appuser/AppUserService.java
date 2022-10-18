78053218a6a1bf05faea7ea7a6889356895e4d98
Juan Bonino <tomasboni9@gmail.com>
2022-10-17 23:32:17 -0300

    feature: added EmailSender - send confirmation token via email

--- src/main/java/com/boninojuan/demo/appuser/AppUserService.java
+++ src/main/java/com/boninojuan/demo/appuser/AppUserService.java
@@ -54,8 +54,6 @@ public class AppUserService implements UserDetailsService {
 
     confirmationTokenService.saveConfirmationToken(confirmationToken);
 
-    // TODO : Send email
-
     return token;
 
     }
